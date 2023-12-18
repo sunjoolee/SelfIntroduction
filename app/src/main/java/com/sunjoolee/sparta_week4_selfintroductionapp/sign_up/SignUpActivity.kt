29e706239542fe25220c8cda658c56bf15374ca1
sunjoolee <sheisthepoem@naver.com>
2023-12-18 20:28:17 +0900

    테스트 중 - time out 발생
    Input dispatching timed out

--- app/src/main/java/com/sunjoolee/sparta_week4_selfintroductionapp/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/sunjoolee/sparta_week4_selfintroductionapp/sign_up/SignUpActivity.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
 import com.sunjoolee.sparta_week4_selfintroductionapp.ActivityCode
 import com.sunjoolee.sparta_week4_selfintroductionapp.R
+import com.sunjoolee.sparta_week4_selfintroductionapp.extentions.setInvisible
+import com.sunjoolee.sparta_week4_selfintroductionapp.extentions.setVisible
 import com.sunjoolee.sparta_week4_selfintroductionapp.text_watchers.EmailTextWatcher
 import com.sunjoolee.sparta_week4_selfintroductionapp.text_watchers.NameTextWatcher
 import com.sunjoolee.sparta_week4_selfintroductionapp.text_watchers.PasswordTextWatcher
@@ -29,12 +31,14 @@ class SignUpActivity : AppCompatActivity() {
     private val passwordCheckEditText by lazy{ findViewById<EditText>(R.id.et_password_check)}
     private val passwordCheckWarningTextView by lazy{ findViewById<TextView>(R.id.tv_password_check_warning)}
 
+    private val signUpWarningTextView by lazy{ findViewById<TextView>(R.id.tv_sign_up_warning) }
     private val signUpButton by lazy{ findViewById<Button>(R.id.btn_sign_up)}
 
     companion object{
         var isNameValid = false
         var isEmailValid = false
         var isPasswordValid = false
+        var isPasswordCheckValid = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,13 +52,34 @@ class SignUpActivity : AppCompatActivity() {
         passwordEditText.addTextChangedListener(
             PasswordTextWatcher(passwordWarningTextView, ActivityCode.SIGN_UP))
         passwordCheckEditText.addTextChangedListener{
-
+            //비어있는지 확인
+            if(it.isNullOrBlank()){
+                passwordCheckWarningTextView.apply{
+                    text = resources.getString(R.string.empty_password_warning)
+                    setVisible()
+                    isPasswordCheckValid = false
+                }
+            }
+            //비밀번호와 일치하는지 확인
+            else if(it.toString() != passwordEditText.text.toString()){
+                passwordCheckWarningTextView.apply{
+                    text = resources.getString(R.string.password_different_warning)
+                    setVisible()
+                    isPasswordCheckValid = false
+                }
+            }
+            else{
+                passwordCheckWarningTextView.setInvisible()
+                isPasswordCheckValid = true
+            }
         }
 
         signUpButton.setOnClickListener {
             //회원가입하기 위해 모든 입력 유효한지 확인
-            if(isNameValid && isEmailValid && isPasswordValid) {
+            if(isNameValid && isEmailValid && isPasswordValid && isPasswordCheckValid) {
                 Log.d(TAG, "sign up button) sign up success")
+                signUpWarningTextView.setInvisible()
+
                 //회원가입
                 val name = nameEditText.text.toString()
                 val email = emailEditText.text.toString()
@@ -66,6 +91,12 @@ class SignUpActivity : AppCompatActivity() {
             }
             else{
                 Log.d(TAG, "sign up button) sign up fail")
+                signUpWarningTextView.setVisible()
+
+                if(!isNameValid) Log.d(TAG, "sign up button) name not valid")
+                if(!isEmailValid) Log.d(TAG, "sign up button) email not valid")
+                if(!isPasswordValid) Log.d(TAG, "sign up button) password not valid")
+                if(!isPasswordCheckValid) Log.d(TAG, "sign up button) password check not valid")
             }
         }
     }
